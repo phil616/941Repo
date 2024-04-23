47105c596db34af8f448f1bd7f9d0d4617311d3f
phil616 <phil616@163.com>
2024-04-23 22:49:07 +0800

    add linked stack

--- src/c35_linked_stack.c
+++ src/c35_linked_stack.c
@@ -18,14 +18,52 @@ struct linked_stack_node
 };
 typedef struct linked_stack_node node;
 
-node* create();
-void pop(node* s);
-void push(datatype data,node *s);
-datatype peek(node *s);
-void clear(node *s);
-int isEmpty(node *s);
+node* create(){
+    node* new = (node*)malloc(sizeof(node));
+    new->next = NULL;
+    return new;
+}
+node* pop(node* s){
+    node* p = NULL;
+    if (s->next != NULL){
+        p = s->next;
+        free(s);
+    }
+    return p;
+}
+node* push(datatype data,node *s){
+    node* new = create();
+    new->data = data;
+    new->next = s;
+    return new;
+}
+datatype peek(node *s){
+    return s->data;
+}
+void clear(node *s){
+    node *p = NULL;
+    p = s;
+    node* sentinel = s;
+    while(p != NULL){
+        sentinel = p;
+        p = p->next;
+        free(sentinel);
+    }
+}
+int isEmpty(node *s){
+    if (s->next != NULL){
+        return 0;
+    }else{
+        return 1;
+    }
+}
 
 int main(){
+    node * stack = NULL;
+    stack = push(1,stack);
+    stack = push(2,stack);
+    printf("top = %d\n",peek(stack));
 
+    // all done
 }
 
